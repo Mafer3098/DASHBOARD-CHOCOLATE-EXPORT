fb0c9bf9e7ae18c2331cff5afeb45d087d591460
Mafer3098 <maria.rodriguez.saenz@est.una.ac.cr>
2025-05-19 20:09:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import matplotlib
+matplotlib.use('Agg')  # Evita errores en entornos sin GUI
 import matplotlib.pyplot as plt
 
 # URLs de los archivos CSV
